2e3d0c106e6827564a34aeac785b083fd28b22a3
deimar <deimarparn14@gmail.com>
2025-01-14 23:49:19 +0200

    Modified sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import '../scss/Sidebar.scss';
 import { Drawer, List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
-import { useNavigate } from 'react-router';
+import CampaignIcon from '@mui/icons-material/Campaign';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
 
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const handleHome = () => {
     navigate(`/home`);
@@ -18,14 +21,23 @@ const Sidebar = () => {
   return (
     <Drawer variant="permanent" anchor="left">
       <List>
-        <ListItem onClick={handleHome}>
+        <ListItem 
+            onClick={handleHome}
+            className={location.pathname === "/home" ? "current" : ""}
+        >
             <ListItemIcon>
                 <HomeIcon />
             </ListItemIcon>
-          <ListItemText primary="Home"/>
+            <ListItemText primary="Home"/>
         </ListItem>
-        <ListItem onClick={handleCampaigns}>
-          <ListItemText primary="Campaigns"/>
+        <ListItem 
+            onClick={handleCampaigns}
+            className={location.pathname === "/campaigns" ? "current" : ""}
+        >
+            <ListItemIcon>
+                <CampaignIcon />
+            </ListItemIcon>
+            <ListItemText primary="Campaigns"/>
         </ListItem>
       </List>
     </Drawer>
